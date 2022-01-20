6ffe142cbf6549bd3f0f49a04562cf99e547446b
Macbook <tahreer.90z@gmail.com>
2022-01-20 19:14:35 +0300

    partially done

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+const Home = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 30);
+    // counter++ => counter = counter + 1
+    //console.log("hi");
+  };
+  const purchase = () => {
+    setCounter(counter - 50);
+  };
+  return (
+    <div>
+      <h1>Welcome To The Clicking Game</h1>
+      <h2>{counter}</h2>
+      <button className="button" onClick={incrementCounter}>
+        Click
+      </button>
+      <button className="button" onClick={purchase}>
+        Purchae Building
+      </button>
+    </div>
+  );
+};
+
+export default Home;
